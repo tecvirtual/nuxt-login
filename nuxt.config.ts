f7fd3bdcab23be4bb3e4d70dae4bf9cd1f9624ac
Frank Garcia <garcia1901l@gmail.com>
2023-07-23 21:54:25 -0500

    api login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/bootstrap.min.css']
+  css: ['~/assets/css/bootstrap.min.css'],
+  modules: ['nuxt-api-party'],
+  runtimeConfig: {
+    apiParty: {
+      endpoints: {
+        jsonPlaceholder: {
+          url: 'https://dummyjson.com', // Reemplaza esto con la URL base de tu API
+          token: '' // Deja el token en blanco por ahora, ya que se generará dinámicamente
+        }
+      }
+    }
+  }
 
 })
